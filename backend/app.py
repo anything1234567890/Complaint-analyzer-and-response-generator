83a0000e870be0cea3cd1ad52997ec847c5882c4
anything1234567890 <icchbin18@gmail.com>
2025-07-09 02:44:42 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -58,6 +58,7 @@ Complaint:
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ["PORT"])  # remove default 5000
     app.run(host='0.0.0.0', port=port)
+
 
